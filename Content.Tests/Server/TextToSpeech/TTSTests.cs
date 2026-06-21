7405e52f6b6ff18542cb299fed2a344f6c8a9aa8
Tim Blyth <timmblyth@gmail.com>
2026-06-21 17:30:55 +1200

    Add unit tests for TTS

---
+++ Content.Tests/Server/TextToSpeech/TTSTests.cs
@@ -0,0 +1,38 @@
+using Content.Server._Starlight.TextToSpeech;
+using NUnit.Framework;
+
+namespace Content.Tests.Server.TextToSpeech;
+
+[TestFixture]
+public sealed class TTSTests
+{
+    [Test]
+    public void TestCleanTextPreservesApostrophes()
+    {
+        // Contractions
+        Assert.That(TTSSystem.CleanText("I'm here"), Is.EqualTo("I'm here"));
+        Assert.That(TTSSystem.CleanText("don't stop"), Is.EqualTo("don't stop"));
+        Assert.That(TTSSystem.CleanText("won't stop"), Is.EqualTo("won't stop"));
+
+        // Accent ending/beginning apostrophes
+        Assert.That(TTSSystem.CleanText("thinkin'"), Is.EqualTo("thinkin'"));
+        Assert.That(TTSSystem.CleanText("'ello"), Is.EqualTo("'ello"));
+    }
+
+    [Test]
+    public void TestCleanTextNormalizesSmartQuotes()
+    {
+        // Smart quote normalization
+        Assert.That(TTSSystem.CleanText("I’m here"), Is.EqualTo("I'm here"));
+        Assert.That(TTSSystem.CleanText("don’t stop"), Is.EqualTo("don't stop"));
+        Assert.That(TTSSystem.CleanText("‘ello"), Is.EqualTo("'ello"));
+    }
+
+    [Test]
+    public void TestCleanTextStripsInvalidCharacters()
+    {
+        // Standard stripping behavior
+        Assert.That(TTSSystem.CleanText("[color=red]Hello[/color] world!"), Is.EqualTo("Hello world!"));
+        Assert.That(TTSSystem.CleanText("Hello @#$% world!"), Is.EqualTo("Hello  world!"));
+    }
+}
